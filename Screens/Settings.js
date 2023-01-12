7a5d90d91ef1e06120d1a26108715897b59d06f9
tokziqijocelyn <115977531+tokziqijocelyn@users.noreply.github.com>
2023-01-12 22:16:04 +0800

    almost?

--- Screens/Settings.js
+++ Screens/Settings.js
@@ -1,168 +1,105 @@
-import React, { useState } from "react";
-import { Component } from 'react';
+import React, { Component }from 'react';
 import { FlatList, StyleSheet, Text, View, Switch } from 'react-native';
 import {
   Ionicons,
+  FontAwesome,
+  MaterialIcons,
   AntDesign,
+  Octicons,
   MaterialCommunityIcons
 } from '@expo/vector-icons'
+import { useState } from "react";
 
-const musicName = [ 'Forest','Waves', 'Rain','Birds'];
+const MoonIcon = ({ focused, color, size }) => {
+  return <MaterialCommunityIcons name="weather-night" size={24} color={"black"} />;
+};
+
+const BellIcon = ({ focused, color, size }) => {
+  return <MaterialCommunityIcons name="bell-badge" size={24} color={"black"} />;
+};
 
 
-const App = () => {
-
-  const [SelectMusic, SetSelectMusic] = React.useState(musicName[0]);
 
 
-  const NextPress = () => {
-    if (SelectMusic.id !== 4) {
-      let temp = musicName.find((c) => c.id === SelectMusic.id + 1);
-      if (temp) {
-        SetSelectMusic(temp);
-      }
+
+class SettingsList extends Component {
+  constructor() {
+    super();
+    this.state = {
+       listKeys: [
+      {key: 'Night Mode', switch : false},
+      {key: 'Notification', switch : false}
+      ]
     }
-  };
+  }
 
-  const PrevPress = () => {
-    if (SelectMusic.id !== 1) {
-      let temp = musicName.find((c) => c.id === SelectMusic.id - 1);
-      if (temp) {
-        SetSelectMusic(temp);
-      }
-    }
-  };
+  setSwitchValue = (val, ind) => {
+      const tempData = _.cloneDeep(this.state.listKeys);
+      tempData[ind].switch = val;
+      this.setState({ listKeys: tempData });
+  }
 
-  const [isEnabled, setIsEnabled] = useState(false);
-  const [isEnabled2, setIsEnabled2] = useState(false);
-  
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
-  const toggleSwitch2 = () => setIsEnabled2(previousState => !previousState);
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.container2}>
-      <MaterialCommunityIcons name="weather-night" size={35} color={"black"} style={styles.icon1}  />
-      <MaterialCommunityIcons name="bell-badge" size={30} color={"black"}  />
-      <Text style={styles.title1}>Dark Mode</Text>
-      <Text style={styles.title2} >Notifications</Text>
+  listItem = ({item, index}) => (
+    <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems:"center", marginHorizontal: 30, paddingLeft:40, paddingRight:30}}>
+      <Text style={styles.item}>{item.key}</Text>
       <Switch
-        style={styles.switch1}
+        
         trackColor={{ false: "#2A0052", true: "#2A0052" }}
         thumbColor= "#E9DCFF" 
         activeThumbColor= "#E9DCFF"
-        onValueChange={toggleSwitch2}
-        value={isEnabled2}
+     
       />
-      <Switch
-        style={styles.switch2}
-        trackColor={{ false: "#2A0052", true: "#2A0052" }}
-        thumbColor= "#E9DCFF"
-        activeThumbColor= "#E9DCFF"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
-      <Ionicons name="musical-notes" size={32} color="black"  />
-      <Text style={styles.title3} >Music</Text>
-      <AntDesign
-          name="caretleft"
-          size={15}
-          color="black"
-          style={styles.icon4}
-          onPress={() => PrevPress()}
-        />
-
-        <AntDesign
-          name="caretright"
-          size={15}
-          color="black"
-          style={styles.icon5}
-          onPress={() => NextPress()}
-        />
-        </View>
     </View>
   );
+  
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+  
+
+      <MaterialCommunityIcons name="weather-night" size={24} color={"black"} />
+      <MaterialCommunityIcons name="bell-badge" size={24} color={"black"} />
+      <FlatList
+        
+        data={this.state.listKeys}
+        renderItem={this.listItem}
+      />
+
+      
+      
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,    
-    alignItems: "space-evenly",
-    backgroundColor:"#ffffff",
-    
+   flex: 1,
+   paddingTop: 0,
+    justifyContent: 'center'
+
   },
-  container2 :{
-    backgroundColor:"blue",
-    paddingLeft:50,
-    
-    
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
-  icon1:{
-    
-    
-    
-  },
-  icon2:{
-    height: 0,
-    position:'absolute',
-    top:-160,
-    left:53
-  },
-  title1:{
-    position:"absolute",
-    top:8,
-    paddingLeft:110,
-    fontSize: 20,
-    color: '#000000',
-    fontWeight:'bold',
-    fontFamily:'Lexend-Medium'
-  },
-  title2:{
-    position:"absolute",
-    top:39,
-    paddingLeft:110,
-    fontSize: 20,
-    color: '#000000',
-    fontWeight:'bold',
-    fontFamily:'Lexend-Medium'
-  },
-  title3:{
-      position:"absolute",
-      fontSize: 20,
-    color: '#000000',
-    fontWeight:'bold',
-    fontFamily:'Lexend-Medium',
-    paddingLeft:110,
-    top:70
-  },
-  switch1:{
-    position:"absolute",
-    top:0,
-    paddingLeft:250,
-    
-  },
-  switch2:{
-    position:"absolute",
-    top:30,
-    paddingLeft:250
-    
-  },
-  icon3:{
+  icon1: {
+
     height: 0,
     position:'relative',
-    top:-198,
-    left:52
+    top:10,
+    left:50
   },
-  icon5:{
-    position:"absolute",
-    top:78,
-    paddingLeft:280
+  icon2: {
+    padding: 0,
+    height: 0,
+    position:'relative',
+    top:54,
+    left:50
+    
   },
-  icon4:{
-    position:"absolute",
-    top:78,
-    paddingLeft:220
-  }
-});
-
-export default App;
+})
+export default SettingsList;
